86017a116777a462868ca102ba0e141846f6f46e
Шепель Ярослав Валерійович <160532292+yaroslavshepel@users.noreply.github.com>
2024-09-10 08:31:35 +0300

    new lab

--- Lab1/Lab1/obj/Debug/net8.0/Lab1.AssemblyInfo.cs
+++ Lab1/Lab1/obj/Debug/net8.0/Lab1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Lab1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dcc2977a030c708cb946a8784dbae72f33197a73")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+299c666a1661ca740317d1431956d922691d03e3")]
 [assembly: System.Reflection.AssemblyProductAttribute("Lab1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Lab1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
